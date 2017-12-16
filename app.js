4feca0f76d1ec5dcb6590fdc6e1b514234c6cffc
mac craig <maccraig98@gmail.com>
2017-12-16 09:54:07 -0800

    Add user functionality added

--- app.js
+++ app.js
@@ -3,27 +3,29 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
+var bodyParser = require("body-parser");
 
 var cors = require('cors');
-var users = require('./routes/users');
 
 var app = express();
 
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json())
 app.use(cors());
-app.use(bodyParser.json());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+var index = require('./routes/index');
+var users = require('./routes/users');
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
