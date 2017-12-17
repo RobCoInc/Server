d3bb9d7f7a680cef6f171992612174d7ca332d90
mac craig <maccraig98@gmail.com>
2017-12-17 00:13:27 -0800

    Add base files for company table

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.set('view engine', 'pug');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var companys = require('./routes/companys');
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/api/users', users);
+app.use('/api/companys', companys);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ models/company.js
@@ -0,0 +1,19 @@
+var db = require('../connect');
+var company = {
+    getAllCompanys: function(callback) {
+        return db.query("select * from company", callback);
+    },
+    getCompanyById: function(id, callback) {
+        return db.query("select * from company where company_id=?", [id], callback);
+    },
+    addCompany: function(company, callback) {
+        return db.query("Insert into company values(?,?,?)", [company.id, company.first, company.last], callback);
+    },
+    deleteCompany: function(id, callback) {
+        return db.query("delete from company where company_id=?", [id], callback);
+    },
+    updateCompany: function(id, company, callback) {
+        return db.query("update company set first=?,last=? where company_id=?", [company.first, company.last, id], callback);
+    }
+};
+module.exports = company;
