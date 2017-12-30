52994fb2448e551a0d987a8487bfc15e78cefdda
Mackenzie Craig <maccraig98@gmail.com>
2017-12-29 17:05:47 -0800

    Add base api for Description, Location, Shift

---
+++ models/shift.js
@@ -0,0 +1,23 @@
+//
+// This file contains functions called from routes/shifts.js
+//
+
+var db = require('../connect');
+var shift = {
+    getAllShifts: function(callback) {
+        return db.query("select * from shift", callback);
+    },
+    getShiftById: function(id, callback) {
+        return db.query("select * from shift where _id=?", [id], callback);
+    },
+    addShift: function(shift, callback) {
+        return db.query("insert into shift (reason, complacency, time, date, locationId, descriptionId, userId)", [shift.reason, shift.complacency, shift.time, shift.date, shift.locationId, shift.descriptionId, shift.userId], callback);
+    },
+    deleteShift: function(id, callback) {
+        return db.query("delete from shift where _id=?", [id], callback);
+    },
+    updateShift: function(id, shift, callback) {
+        return db.query("update shift set first=?,last=? where company_id=?", [shift.first, shift.last, id], callback);
+    }
+};
+module.exports = shift;
---
+++ routes/locations.js
@@ -0,0 +1,32 @@
+//
+// This file handles the routing for "location" API requests.
+//
+
+var express = require('express');
+var router = express.Router();
+var location = require('../models/location');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+router.get('/', function(req, res, next) {
+      location.getAllLocations(function(err, rows) {
+          if (err) {
+              res.json(err);
+          } else {
+              res.json(rows);
+          }
+      });
+});
+
+router.post('/', function(req, res) {
+    location.addLocation(req.body, function(err, count) {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(req.body);
+        }
+    });
+});
+
+module.exports = router;
