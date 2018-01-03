084e00710b073f8b8f359b3b81446ea7e924034c
Mackenzie Craig <maccraig98@gmail.com>
2018-01-03 09:55:18 -0800

    Add description routes (no functions yet)

--- routes/descriptions.js
+++ routes/descriptions.js
@@ -19,6 +19,45 @@ router.get('/', function(req, res, next) {
       });
 });
 
+
+// TODO: Make the following 3 routes
+
+router.get('/getDescriptionsByName/:id?', function(req, res, next) {
+  if (req.params.email) {
+      description.getDescriptionsByName(req.params.id, function(err, rows) {
+          if (err) {
+              res.json(err);
+          } else {
+              res.json(rows);
+          }
+      });
+    }
+});
+
+router.get('/getDescriptionsByNameWhereUserIdMatches/:id?', function(req, res, next) {
+  if (req.params.email) {
+      description.getDescriptionsByNameWhereUserIdMatches(req.params.id, function(err, rows) {
+          if (err) {
+              res.json(err);
+          } else {
+              res.json(rows);
+          }
+      });
+    }
+});
+
+router.get('/getDescriptionsByNameWhereCompanyIdMatches/:id?', function(req, res, next) {
+  if (req.params.email) {
+      description.getDescriptionsByNameWhereCompanyIdMatches(req.params.id, function(err, rows) {
+          if (err) {
+              res.json(err);
+          } else {
+              res.json(rows);
+          }
+      });
+    }
+});
+
 router.post('/', function(req, res) {
     description.addDescription(req.body, function(err, count) {
         if (err) {
